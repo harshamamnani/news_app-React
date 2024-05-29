be9d4d60c1107aea01acb357ba12dae30f6c1b20
harshamamnani <76839912+harshamamnani@users.noreply.github.com>
2024-05-29 23:15:14 +0530

    Add files via upload

---
+++ src/components/NewsItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const NewsItem =(props) => {
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } =props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger">{source}</span>
+          </div>
+          <img
+            src={
+              !imageUrl
+                ? "https://us.123rf.com/450wm/bluebay/bluebay1409/bluebay140900035/31543593-words-news-on-digital-red-background.jpg?ver=6"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+export default NewsItem;
